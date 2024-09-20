7c3b926f09dbda22ba7e93224ecacdc4b0c2d445
Filip Mossberg <filip.mossberg@consid.se>
2024-09-20 16:00:28 +0200

    Astro islands for interactive elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-exploring.vercel.app/"
+  site: "https://astro-exploring.vercel.app/",
+  integrations: [preact()]
 });
-
